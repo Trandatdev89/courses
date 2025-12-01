4bd3471bcd2178a0a1cd3863583fb1826b6cf8b9
Trandatdev89 <tranquocdat30122004@gmail.com>
2025-12-01 21:22:00 +0700

    fix v4

--- src/main/java/com/project01/skillineserver/vnpay/VNPayConfig.java
+++ src/main/java/com/project01/skillineserver/vnpay/VNPayConfig.java
@@ -15,8 +15,8 @@ import java.util.*;
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_Returnurl = "/vnpay-payment/";
-    public static String vnp_TmnCode = "GQVL34ZT";
-    public static String vnp_HashSecret = "QSCWGU1WJVH7TYMV91TXDE86RI6ES2M9";
+    public static String vnp_TmnCode = "7YDV4IN6";
+    public static String vnp_HashSecret = "3LW9NCWFQN0A4W1SJXS7AQZ10PUJODH4";
     public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
