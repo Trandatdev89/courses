57436c9c55da50709c1a8b42b248ed7a7a87ed0b
Trandatdev89 <tranquocdat30122004@gmail.com>
2026-02-09 14:02:14 +0700

    simulation attack website csrf

--- src/main/java/com/project01/skillineserver/config/WebSecurityConfig.java
+++ src/main/java/com/project01/skillineserver/config/WebSecurityConfig.java
@@ -117,6 +117,7 @@ public class WebSecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedOrigin("http://localhost:63342");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setAllowCredentials(true);
